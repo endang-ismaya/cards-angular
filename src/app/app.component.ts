54481d0a6a84bc3c46ba94c257ecc74ef54ad066
Endang.Ismaya <endang.ismaya@gmail.com>
2020-07-10 21:50:31 +0700

    20200710: card continue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'Saw this awesome tree during my hike today',
+      imageUrl: 'assets/img/tree.jpeg',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+      imageUrl: 'assets/img/mountain.jpeg',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking1234',
+      content: 'I did some biking today, yihaa!!!',
+      imageUrl: 'assets/img/biking.jpeg',
+    },
+  ];
 }
